a04713a35f27c0961d2d19814a9c567d891b2779
Shodai-Takehara <higo.hh.bb.moccos@gmail.com>
2024-07-30 22:28:19 +0900

    Add: Headコンポーネントを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import Link from 'next/link';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <section>
         <h1>
           Read <Link href="/posts/first-post">this page!</Link>
--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,9 +1,20 @@
 import type { FC } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
 
 const FirstPost: FC = () => {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Image
+        src="/images/profile.jpg" // イメージファイルのルート
+        height={144} // イメージサイズの設定
+        width={144} // アスペクト比を正しく
+        alt="Your Name"
+      />
       <h1>First Post</h1>
       <h2>
         <Link href="/">← Back to home!!</Link>
